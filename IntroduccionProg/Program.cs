db564110be0ecb82fa6a301e23e7cc01e3f54541
GutierrezEscudero <gutierrezEscudero@outlook.com>
2026-09-14 10:28:44 +0200

    Carpeta proyecto y primer ejercicio

---
+++ IntroduccionProg/Program.cs
@@ -0,0 +1,11 @@
+﻿//NIVEL 1 - MOSTRAR INFORMACIÓN
+
+//Ejercicio 1 - Presentación
+
+
+string nombre = "Beatriz";
+const string CIUDAD = "Socuéllamos";
+int edad = 27;
+string comidaFavorita = "macarrones";
+
+Console.WriteLine($"Hola, me llamo {nombre}, y vivo en {CIUDAD}. \nTengo {edad} años, y mi comida favorita es un plato de {comidaFavorita}. ");
