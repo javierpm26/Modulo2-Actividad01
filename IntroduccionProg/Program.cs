cd89ae7fed3de72b4c8b2605b85df4622551a353
javierpm26 <javierparra909@gmail.com>
2026-09-14 10:54:55 +0200

    Ejercicio 2 hecho

--- IntroduccionProg/Program.cs
+++ IntroduccionProg/Program.cs
@@ -9,3 +9,15 @@ int edad = 27;
 string comidaFavorita = "macarrones";
 
 Console.WriteLine($"Hola, me llamo {nombre}, y vivo en {CIUDAD}. \nTengo {edad} años, y mi comida favorita es un plato de {comidaFavorita}. ");
+
+
+//Ejercicio 2 - Datos básicos
+string nombre2 = "Javier";
+int edad2 = 25;
+double altura = 1.72;
+bool estudiante = true;
+
+Console.WriteLine($"Hola me llamo {nombre2}, y tengo {edad2} años. Mido {altura} metros");
+
+Console.WriteLine($"¿Soy estudiante?{estudiante}");
+
