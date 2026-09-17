789283bbd610b2ce4c1cf96887ab5cb19461230f
javierpm26 <javierparra909@gmail.com>
2026-09-17 10:52:36 +0200

    feat: Ejercicio 4 completado

--- Modulo2-Actividad03/FuncionesExcepcionesFicheros/Program.cs
+++ Modulo2-Actividad03/FuncionesExcepcionesFicheros/Program.cs
@@ -1,7 +1,7 @@
 ﻿// BLOQUE 1 - FUNCIONES
 
 // Ejercicio 1 - Primera función
-static void MostrarMensaje()
+/*static void MostrarMensaje()
 {
     Console.WriteLine("Bienvenido al programa");
 }
@@ -24,4 +24,17 @@ static int Sumar(int numA, int numB)
     return numA + numB;
 }
 
-Console.WriteLine($"El resultado de la suma es {Sumar(5,10)}");
+Console.WriteLine($"El resultado de la suma es {Sumar(5,10)}");*/
+
+
+// Ejercicio 4 - Varios parámetros y retorno
+
+static double CalcularMedia(double num1, double num2, double num3)
+{
+    return (num1 + num2 + num3) / 3;
+}
+Console.WriteLine($"La media es {CalcularMedia(5.0, 2.5, 3.75)}");
+Console.WriteLine($"La media es {CalcularMedia(3.0, 2.5, 10)}");
+
+
+
