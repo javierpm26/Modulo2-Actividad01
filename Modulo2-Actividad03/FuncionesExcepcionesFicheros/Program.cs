ec6df88c5668095e05ff5f97a2672dda8e2614b8
javierpm26 <javierparra909@gmail.com>
2026-09-17 11:30:06 +0200

    feat: Ejercicio 6 completado

--- Modulo2-Actividad03/FuncionesExcepcionesFicheros/Program.cs
+++ Modulo2-Actividad03/FuncionesExcepcionesFicheros/Program.cs
@@ -1,7 +1,7 @@
 ﻿// BLOQUE 1 - FUNCIONES
 
 // Ejercicio 1 - Primera función
-static void MostrarMensaje()
+/*static void MostrarMensaje()
 {
     Console.WriteLine("Bienvenido al programa");
 }
@@ -49,4 +49,22 @@ MostrarAmbito();
 
 //Esto daría error:
 //Console.WriteLine(numeroLocal);
+
+
+// BLOQUE 2 - PASO DE PARÁMETROS Y RECURSIVIDAD 
+
+// Ejercicio 6 - Paso por valor
+
+int numero = 10; //Esta es la variable número original
+
+static void Cambiar(int numero)
+{
+    numero = 26; // Esta variable numero es una copia que usa la función
+    Console.WriteLine($"Aquí numero tiene un valor de {numero}"); // Muestra lo que vale la copia dentro de la función
+
+}
+Cambiar(numero); // Llamar a la función hace una copia de la variable numero al que machaca su valor que es 10 a 26
+Console.WriteLine($"Numero aquí vale {numero}");*/
+
+
 
